e453224a131541da71f814e5540cafb22492ecf0
gyoon <inticoy0406@gmail.com>
2024-05-07 22:59:18 +0900

    Feat: Login Test with JWT

--- backend/django/reflected/user/urls.py
+++ backend/django/reflected/user/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
 
+from .views import UserTestView
+
+
+# host/auth/
+urlpatterns = [
+    path("test/", UserTestView.as_view()),
+]
--- backend/django/reflected/user/views.py
+++ backend/django/reflected/user/views.py
@@ -1,1 +1,11 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView, status
+
+
+class UserTestView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request: Request):
+        return Response("logined!", status=status.HTTP_200_OK)
