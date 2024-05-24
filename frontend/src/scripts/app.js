30ff1ea4f77bb7dc151f4e6d05190bfb0ee3a712
inticoy <inticoy0406@gmail.com>
2024-05-24 15:42:28 +0900

    Feat: navbar button

--- frontend/src/scripts/app.js
+++ frontend/src/scripts/app.js
@@ -1,5 +1,5 @@
 import { switchApp } from "/src/scripts/utils/display.js";
-import { login } from "/src/scripts/view/login.js";
+import { login, setLogin } from "/src/scripts/view/login.js";
 import { setNavbar } from "/src/scripts/view/navbar.js";
 import { setHome } from "/src/scripts/view/home.js";
 
