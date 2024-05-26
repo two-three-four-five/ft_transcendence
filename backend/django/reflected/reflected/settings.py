02c5b7bed30675459354ae526ecdfee28b7327d8
gyoon <inticoy0406@gmail.com>
2024-05-26 20:47:50 +0900

    Feat: postgres

--- backend/django/reflected/reflected/settings.py
+++ backend/django/reflected/reflected/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = "reflected.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
     }
 }
 
